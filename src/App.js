24ecf59a13a853da18f43d75d931d8d8721a58f4
woopi1030 <lozenkrantz@gmail.com>
2025-01-23 00:30:19 +0900

    [ADD] 샘플 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React 샘플 페이지</h1>
+        <p>React로 첫 페이지를 만들어 봅시다!</p>
       </header>
     </div>
   );
