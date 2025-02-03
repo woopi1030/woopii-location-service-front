14b12f9b733e73a137e3d574d1d3566e44a42a34
woopi1030 <lozenkrantz@gmail.com>
2025-02-04 00:59:50 +0900

    [ADD] 마커 추가

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet"; // leaflet import 추가
 import "leaflet/dist/leaflet.css";
 import { getPlaces } from "../services/firestore";
 
 const center = [37.5665, 126.9780]; // 기본 중심 위치 (서울)
+
+// 📌 커스텀 아이콘 설정
+const customIcon = L.icon({
+  iconUrl: "/free-icon-location-marker.png", // public 폴더 내 아이콘 경로
+  iconSize: [32, 32], // 아이콘 크기 조절
+  iconAnchor: [16, 32], // 아이콘의 기준점 (중앙 하단)
+  popupAnchor: [0, -32], // 팝업 위치 조정
+});
 
 const MapComponent = () => {
   const [places, setPlaces] = useState([]);
@@ -23,7 +32,7 @@ const MapComponent = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {places.map((place) => (
-        <Marker key={place.id} position={[place.lat, place.lng]}>
+        <Marker key={place.id} position={[place.lat, place.lng]} icon={customIcon}>
           <Popup>{place.name}</Popup>
         </Marker>
       ))}
